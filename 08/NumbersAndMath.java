bb7aab9df0662c2651361f64053ec3b137242d91
perrywu13 <36411089+perrywu13@users.noreply.github.com>
2018-02-16 09:33:44 -0500

    pbd-solution folder 02-07

---
+++ 08/NumbersAndMath.java
@@ -0,0 +1,30 @@
+public class NumbersAndMath
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "I will now count my chickens:" ); // the purpose of this code 
+
+		System.out.println( "Hens " + ( 25 + 30 / 6 ) ); // count the hens
+		System.out.println( "Roosters " + ( 100 - 25 * 3 % 4 ) ); // count the rooster
+
+		System.out.println( "Now I will count the eggs:" ); // count the eggs
+
+		System.out.println( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 ); // counting eggs
+
+		System.out.println( "Is it true that 3 + 2 < 5 - 7?" ); // asking a question
+
+		System.out.println( 3 + 2 < 5 - 7 ); // checking the math
+
+		System.out.println( "What is 3 + 2? " + ( 3 + 2 ) ); //ask how to math
+		System.out.println( "What is 5 - 7? " + ( 5 - 7 ) ); //ask how to math
+
+		System.out.println( "Oh, that's why it's false." ); // proving the statement earlier  
+
+		System.out.println( "How about some more." ); // more practices
+
+		System.out.println( "Is it greater? " + ( 5 > -2 ) );
+		System.out.println( "Is it greater or equal? " + ( 5 >= -2 ) );
+		System.out.println( "Is it less or equal? " + ( 5 <= -2 ) );
+	}
+}
+
