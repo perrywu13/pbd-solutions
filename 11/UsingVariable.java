419a2e9c4aeaf5ccf326d385a587cb80d6a97e55
perrywu13 <36411089+perrywu13@users.noreply.github.com>
2018-02-24 19:51:11 -0500

    pbd-solution

---
+++ 11/UsingVariable.java
@@ -0,0 +1,16 @@
+public class UsingVariable
+{
+	public static void main(String[] args)
+	{
+		int x = 113;
+		double y = 2.71828;
+		String text = "Computer Science";
+		
+		System.out.println("This is room # " + x);
+		System.out.println("e is close to " + y);
+		System.out.println("I am learning a bit about " + text);
+	}
+}
+		
+		
+
