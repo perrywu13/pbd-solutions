b6c588b6ea8ade5758247c4593ba574ee285e426
perrywu13 <36411089+perrywu13@users.noreply.github.com>
2018-02-26 16:46:08 -0500

    pbd-solutions

---
+++ 12/StillUsingVariables.java
@@ -0,0 +1,9 @@
+public class StillUsingVariables
+{
+	public static void main(String[] args)
+	{
+		String name = "Perry";
+		int year = 2018;
+		System.out.println("My name is " + name + " and I'll graduate in " + year);
+	}
+}
---
+++ 13/YourSchedule.java
@@ -0,0 +1,21 @@
+public class YourSchedule
+{
+	public static void main(String[] args)
+	{
+		String course1 = "Computer science";
+		String course2 = "Calculus";
+		String course3 = "Morality";
+		String course4 = "English";
+		String teacher1 = "Mr. Gallo";
+		String teacher2 = "Mrs. De Gregorio";
+		String teacher3 = "Mr. Molnar";
+		String teacher4 = "Mr. Sarracini";
+		System.out.println("+-----------------------------------------------------------------+");
+		System.out.println("| 1 |                           " + course1 + "|        " + teacher1 + "|");
+		System.out.println("| 2 |                                   " + course2 + "| " + teacher2 + "|");
+		System.out.println("| 3 |                                   " + course3 + "|       " + teacher3 + "|");
+		System.out.println("| 4 |                                    " + course4 + "|    " + teacher4 + "|");
+		System.out.println("+-----------------------------------------------------------------+");
+	}
+}
+
